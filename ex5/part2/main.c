940751ac07dcc9275ecf005c286866a45fd12abc
amiralimansouri <77664670+amiralimansouri@users.noreply.github.com>
2021-10-29 20:52:18 +0330

    Add files via upload

--- ex5/part2/main.c
+++ ex5/part2/main.c
@@ -1,1 +1,61 @@
+/*
+ * GccApplication6.c
+ *
+ * Created: 2021-10-28 6:55:08 PM
+ * Author : miral
+ */ 
+
+#include <avr/io.h>
+unsigned char move[4] = {0b00000001 , 0b00000010 , 0b00000100 , 0b00001000};
+#include <avr/interrupt.h>
+#include <stdio.h>	
+volatile uint8_t ovf_max;
+int i=-1;
+int j=4;
+
+void timer1_init()
+{
+	TCCR1B |= (1 << CS11);
+	TCNT1 = 0;
+	TIMSK |= (1 << TOIE1);
+	sei();
+	ovf_max = 0;
+}
+
+
+ISR(TIMER1_OVF_vect)
+{
+	
+	ovf_max++;
+	
+	if (~PINC & 0b00000100){
+		i=(i+1)%4;
+		PORTA = move[i];
+	}
+	if (~PIND & 0b00000100){
+		j = (j-1)%4;
+		PORTA = move[j];
+	}
+	
+	
+}
+
+int main(void)
+{
+	DDRA = 0xff;
+	DDRC = 0x00;
+	
+	
+	
+	timer1_init();
+    /* Replace with your application code */
+    while (1) 
+    {
+		//if(ovf_max >= 12){
+	
+		//}
+		
+	
+}
+}
 
